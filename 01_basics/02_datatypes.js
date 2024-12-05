c5fbab4a0dffe57d62c59900679b79ababcaf393
TechwithHaiderAli <hyder6291@gmail.com>
2024-12-05 18:53:37 +0500

    datatyepes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";
+// Treat all js code as newer version
+//alert("Hello")// We are using node js not browser
+console.log(5+5)
+console.log("haider")
+
+let name="Ali"
+let age=19
+let isLogin=true
+let state = null
+//number => 2 to power ( Range of Value)
+//bigint
+//string=>
+// boolean => true or false 
+//null =>standalone value type=>object
+//symbol to find uniqueness 
+//object
+//undefined => typeof undefined
