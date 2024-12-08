aec9cd7f4aa69b16b347b6afb0b50023921ffd1d
TechwithHaiderAli <hyder6291@gmail.com>
2024-12-08 11:22:09 +0500

    DataTyeps Summary

---
+++ 01_basics/data_types_summary.js
@@ -0,0 +1,13 @@
+// Stack and Heap
+
+// For All Primitive Stack is used for memory
+
+//For All Non Primitive Heap is Used for memory
+
+let myYoutube="Hadar Alee"
+
+let anotherName=myYoutube
+
+anotherName="Haider ALi"
+
+console.log(myYoutube,anotherName);
