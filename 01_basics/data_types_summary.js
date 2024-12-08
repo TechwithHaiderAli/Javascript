785f71c83b90ca7ebff8a913d772c2d8cd786248
TechwithHaiderAli <hyder6291@gmail.com>
2024-12-08 11:22:30 +0500

    Data Types Stack/Heap

--- 01_basics/data_types_summary.js
+++ 01_basics/data_types_summary.js
@@ -11,3 +11,6 @@ let anotherName=myYoutube
 anotherName="Haider ALi"
 
 console.log(myYoutube,anotherName);
+
+
+//There are two types of Data types in javascript . One is Primitive and the other is non Primitive. The Primitive types are the building blocks these include : number , string, boolean,null,undefined,Symbol,Bigint. Primitive types are immutable they cannot be changed once created. They are stored in Stack which means whenever variable is initialized and we access it we get a copy of the original one. On the other hand there are non primitive types which includes arrays , functions, objects . These can mutable and they are stored in the hea[ and the reference of heap is stored in the stack .whenver we access these are passed by reference not by the value//
