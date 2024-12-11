9c24cbee87149fa8e6229eead6c519b01328e399
TechwithHaiderAli <hyder6291@gmail.com>
2024-12-11 11:08:47 +0500

    more about functions

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -28,3 +28,41 @@ loginUser(myname)
 // if no argument passed
 
 loginUser()
+
+
+// When you don't know num of parameters
+// we use rest operators
+
+function calculatecartprice(val1,val2,...num1){
+    return num1
+}
+console.log(calculatecartprice(200,400,500));
+
+
+const user={
+    username:"haider",
+    price:999,
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+//handleObject(user)
+
+handleObject({
+    username:"Sam",
+    price:200,
+})
+
+//Passing Arrays to functions
+
+const MyArray=[1,2,3,34,5,5]
+
+function return2ndValue(MyArray){
+
+    return MyArray[4]
+}
+
+//console.log(return2ndValue(MyArray));
+console.log(return2ndValue([1,2,3,45,50,56]));
